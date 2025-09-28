d60f270ceae02d59f6191c422aa5c2a6c63562de
santosh <sharma.santosh2121@gmail.com>
2025-09-28 19:12:46 +0530

    refactor(homepage):replace WebElement click with javaScript click in gotoPimPage

--- src/main/java/com/orangehrm/pages/HomePage.java
+++ src/main/java/com/orangehrm/pages/HomePage.java
@@ -13,13 +13,17 @@ public class HomePage extends BasePage {
 	
 	// page objects
 	private By loginSuccessIndicator = By.xpath("//h6[text()='Dashboard']");
-	
-	
+    private By pimLinkTxt = By.xpath("//ul[@class='oxd-main-menu']//li//span[text() = 'PIM']");
+		
 	// constructor
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
 	
+	public PimPage gotoPimPage() {
+		actionDriver.clickUsingJs(pimLinkTxt);
+		return new PimPage(driver);
+	}	
 		
 	public boolean isLoginSuccessful() {
 		return actionDriver.isDisplayed(loginSuccessIndicator);

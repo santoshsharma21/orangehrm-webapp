6a793b53312d012df876763b563f7f2487f37f18
santosh <sharma.santosh2121@gmail.com>
2025-09-28 20:12:29 +0530

    test(loginpagetest): add multiple login test methods

--- src/test/java/com/orangehrm/tests/LoginPageTest.java
+++ src/test/java/com/orangehrm/tests/LoginPageTest.java
@@ -7,6 +7,7 @@ import com.orangehrm.base.BaseClass;
 import com.orangehrm.dataprovider.TestDataProvider;
 import com.orangehrm.pages.HomePage;
 import com.orangehrm.pages.LoginPage;
+import com.orangehrm.utils.ConfigReader;
 import com.orangehrm.utils.DriverManager;
 import com.orangehrm.utils.FrameworkLogger;
 
@@ -15,14 +16,16 @@ public class LoginPageTest extends BaseClass {
 	// page class instance
 	private LoginPage loginPage;
 	private HomePage homePage;
+	
 
-	@Test(dataProvider = "loginData", dataProviderClass = TestDataProvider.class)
+	@Test(dataProvider = "loginData", dataProviderClass = TestDataProvider.class, groups = {"Positve/Negative Test"})
 	public void loginTest(String username, String password, String expectedResult) {
-		FrameworkLogger.phase("Test started: " + "loginTest");
+		FrameworkLogger.phase("Test started: loginTest");
 		loginPage = new LoginPage(DriverManager.getDriver());
 		homePage = loginPage.login(username, password);
 		
-		FrameworkLogger.info("Test Data => Username: " + username + ", Password: " + password + ", Expected Result: " + expectedResult);
+		String testDataInfo = "Test Data => Username: " + username + ", Password: " + password + ", Expected Result: " + expectedResult;
+		FrameworkLogger.info(testDataInfo);
        
 		if(expectedResult.equalsIgnoreCase("pass")) {
 			Assert.assertTrue(homePage.isLoginSuccessful(), "Login should be successful");
@@ -31,6 +34,38 @@ public class LoginPageTest extends BaseClass {
 			Assert.assertTrue(loginPage.isLoginFailed(), "Login should fail with invalid credentials");
 			FrameworkLogger.info("Login failed as expected");
 		}
-		FrameworkLogger.phase("Test ended: " + "loginTest");
+		FrameworkLogger.phase("Test ended: loginTest");
+	}
+	
+	@Test
+	public void testLoginWithEmptyUsernameField() {
+		FrameworkLogger.phase("Test started: testLoginWithEmptyUsernameField");
+		loginPage = new LoginPage(DriverManager.getDriver());
+		String password = ConfigReader.getProperty("Password");
+		loginPage.login("", password);
+		
+		String testDataInfo = "Test Data => Username: " + "" + ", Password: " + password;
+		FrameworkLogger.info(testDataInfo);
+		
+		Assert.assertTrue(loginPage.isFieldRequiredMessageDisplayed(), "Login should fail with empty username field");
+		FrameworkLogger.info("Login failed as expected");
+		
+		FrameworkLogger.phase("Test ended: testLoginWithEmptyUsernameField");
+	}
+	
+	@Test
+	public void testLoginWithEmptyPasswordField() {
+		FrameworkLogger.phase("Test started: testLoginWithEmptyPasswordField");
+		loginPage = new LoginPage(DriverManager.getDriver());
+		String username = ConfigReader.getProperty("Username");
+		loginPage.login(username, "");
+		
+		String testDataInfo = "Test Data => Username: " + username + ", Password: " + "";
+		FrameworkLogger.info(testDataInfo);
+		
+		Assert.assertTrue(loginPage.isFieldRequiredMessageDisplayed(), "Login should fail with empty password field");
+		FrameworkLogger.info("Login failed as expected");
+		
+		FrameworkLogger.phase("Test ended: testLoginWithEmptyPasswordField");
 	}
 }
